a11ae9dc2e8413b3cf979d048fe8175e1731ac49
priyanshu2304 <priyanshumodi1998@gmail.com>
2024-02-06 09:40:12 +0530

    Lecture - 12

---
+++ components/Flatcards.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Flatcards = () => {
+  return (
+    <View>
+      <Text>Flatcards</Text>
+    </View>
+  );
+};
+
+export default Flatcards;
+
+const styles = StyleSheet.create({});
